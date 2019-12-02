407016ddb595029d103bbd441c95824be49f5d05
daafith <davidbaak@gmail.com>
2019-12-02 21:13:59 +0100

    day 3 prep

---
+++ src/main/kotlin/days/Day03.kt
@@ -0,0 +1,13 @@
+package days
+
+class Day03 : Day(3) {
+
+    override fun partOne(): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun partTwo(): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
