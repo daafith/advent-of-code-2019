e28f949ae86ab8e6921d281b31b60615eaa2f67b
daafith <davidbaak@gmail.com>
2019-12-02 20:55:25 +0100

    naming

--- src/main/kotlin/days/Day01.kt
+++ src/main/kotlin/days/Day01.kt
@@ -3,22 +3,22 @@ package days
 class Day01 : Day(1) {
 
     override fun partOne() : Any {
-        return inputAsList.map { calculateRequiredFuel(it.toInt()) }.sum()
+        return inputAsList.map { requiredFuel(it.toInt()) }.sum()
     }
 
     override fun partTwo(): Any {
-        return inputAsList.map { calculateRequiredFuelIncludingItsOwnMass(it.toInt()) }.sum()
+        return inputAsList.map { requiredFuelCorrected(it.toInt()) }.sum()
     }
 
-    fun calculateRequiredFuel(mass: Int) : Int {
+    fun requiredFuel(mass: Int) : Int {
         return mass / 3 - 2
     }
 
-    fun calculateRequiredFuelIncludingItsOwnMass(mass: Int) : Int {
-        val fuel = calculateRequiredFuel(mass)
+    fun requiredFuelCorrected(mass: Int) : Int {
+        val fuel = requiredFuel(mass)
         return when {
             fuel <= 0 -> 0
-            else -> fuel + calculateRequiredFuelIncludingItsOwnMass(fuel)
+            else -> fuel + requiredFuelCorrected(fuel)
         }
     }
 
--- src/test/kotlin/days/Day01Test.kt
+++ src/test/kotlin/days/Day01Test.kt
@@ -7,17 +7,17 @@ class Day01Test {
 
     @Test
     fun validatePartOne() {
-        assertThat(Day01().calculateRequiredFuel(12)).isEqualTo(2)
-        assertThat(Day01().calculateRequiredFuel(14)).isEqualTo(2)
-        assertThat(Day01().calculateRequiredFuel(1969)).isEqualTo(654)
-        assertThat(Day01().calculateRequiredFuel(100756)).isEqualTo(33583)
+        assertThat(Day01().requiredFuel(12)).isEqualTo(2)
+        assertThat(Day01().requiredFuel(14)).isEqualTo(2)
+        assertThat(Day01().requiredFuel(1969)).isEqualTo(654)
+        assertThat(Day01().requiredFuel(100756)).isEqualTo(33583)
     }
 
     @Test
     fun validatePartTwo() {
-        assertThat(Day01().calculateRequiredFuelIncludingItsOwnMass(14)).isEqualTo(2)
-        assertThat(Day01().calculateRequiredFuelIncludingItsOwnMass(1969)).isEqualTo(966)
-        assertThat(Day01().calculateRequiredFuelIncludingItsOwnMass(100756)).isEqualTo(50346)
+        assertThat(Day01().requiredFuelCorrected(14)).isEqualTo(2)
+        assertThat(Day01().requiredFuelCorrected(1969)).isEqualTo(966)
+        assertThat(Day01().requiredFuelCorrected(100756)).isEqualTo(50346)
     }
 
 }
